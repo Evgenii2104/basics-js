cc6ff28d826db813b918a8be2373a2261555428e
evgenii2104 <evgenii2104@rambler.ru>
2022-12-05 22:06:26 +0300

    rjy

---
+++ lesson6/lesson6.js
@@ -0,0 +1,8 @@
+class Weapon {
+    constructor(title, attackLevel, strength, range) {
+        this.title = title;
+        this.attackLevel = attackLevel;
+        this.strength = strength;
+        this.range = range;
+    }
+}
