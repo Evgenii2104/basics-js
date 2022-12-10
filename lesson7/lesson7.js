89d2fd2915f829df8d1fa6226f680060bb32c2ae
evgenii2104 <evgenii2104@rambler.ru>
2022-12-10 21:11:14 +0300

    f

--- lesson7/lesson7.js
+++ lesson7/lesson7.js
@@ -1,6 +1,57 @@
 // 1 задание
 
-//const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+class Weapon {
+  constructor(name, attackLevel, durability, range) {
+      this.name = name;
+      this.attackLevel = attackLevel;
+      this.durability = durability;
+      this.firstDurability = durability;
+      this.range = range;
+  }
+  takeDamage(damage) {
+      this.durability = this.durability - damage;
+      if (this.durability < 0) {
+          this.durability = 0
+      } 
+
+  }
+  getDamage() {
+      if (this.durability === 0) {
+      console.log(this.attackLevel = 0);
+      } if (this.durability >= ((this.firstDurability / 100) * 30)) {
+          console.log(this.attackLevel)
+      } if (this.durability < ((this.firstDurability / 100) * 30)) {
+          console.log(this.attackLevel / 2);
+      } 
+  }
+  isBroken() {
+      if (this.durability > 0) {
+          console.log(false)
+      } if (this.durability <= 0) {
+          console.log(true)
+      }
+  }
+  getNames() {
+    console.log(this.name)
+  }
+}
+
+
+const weapon1 = new Weapon('Рука', 1, Infinity, 1);
+const weapon2 = new Weapon('Лук', 10, 200, 3);
+const weapon3 = new Weapon('Меч', 25, 500, 1);
+const weapon4 = new Weapon('Нож', 5, 300, 1);
+const weapon5 = new Weapon('Посох', 8, 300,2);
+
+//console.log(weapon1, weapon2, weapon3, weapon4, weapon5)
+
+const bestWeapon1 = new Weapon('Длинный лук ' + weapon2.name, 15, weapon2.durability, 4);
+const bestWeapon2 = new Weapon('Секира ' + weapon3.name, 27, 800, weapon3.range)
+const bestWeapon3 = new Weapon('Посох Бури ' + weapon5.name, 10, weapon5.durability, 3)
+
+//console.log(bestWeapon1, bestWeapon2, bestWeapon3);
+
+//console.log(getNames())
 
 
 
@@ -11,26 +62,26 @@ function sleep(milliseconds)
   let e = new Date().getTime() + milliseconds;
   while (new Date().getTime() <= e) {}
 };
+console.log(sleep(15))
 
-const memory = {
+let memory = {
     arguments: [],
     result: []
 };
 
 function sum(...args) {
     sleep(100);
-    return args.reduce((sum, arg) => {
+    return res = args.reduce((sum, arg) => {
       return sum += +arg;
     }, 0);
-    
-  }; memory.arguments.push(sum)
-
+  }; 
+  
    function memorize(sum, limit) {
-    limit < 5;
-     sum()
+    limit < 5; 
+     sum() 
    }
 
-  function compareArrays( arr1, arr2 ) {
+  function compareArrays( arr1, arr2) {
     return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
   }
 
