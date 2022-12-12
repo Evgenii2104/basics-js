473f06dd495fffac0f9d1dc1cf0557da55155504
evgenii2104 <evgenii2104@rambler.ru>
2022-12-12 23:01:17 +0300

    j

--- lesson7/lesson7.js
+++ lesson7/lesson7.js
@@ -113,24 +113,29 @@ function sum(...args) {
 
 
 function memorize(fn, limit) {
+  let count = 1;
   const memory = {
     arg:[],
     results:[]
   };
-  //if (memory.length <= limit) {
   return (...args) => {
+    if (count <= limit) {
     const result = fn(...args);
     memory.arg.push(args);
     memory.results.push(result);
-    console.log(memory)
-    return result;
+    console.log(memory);
+    count++;
+    return result}
   }
 }
-//}
+
 
 const mSum = memorize(sum, 5);
 //console.log(sum(3, 4));
 //console.log(memory)
 mSum(3, 4);
 mSum(6, 2);
-mSum(4, 7)
+mSum(4, 7);
+mSum(5, 5);
+mSum(6, 6);
+mSum(8, 3);
