186415913debd1fba4ba60107a6a085ce4453e61
evgenii2104 <evgenii2104@rambler.ru>
2023-01-10 23:16:32 +0300

    l

---
+++ lesson01/lesson01.js
@@ -0,0 +1,23 @@
+class UserForm {
+    constructor() {
+         
+    }
+    loginFormCallback(data) {
+
+    }
+    registerFormCallback() {
+
+    }
+}
+
+class ApiConnector {
+    constructor() {
+
+    }
+    login({login, password}, callback) {
+
+    }
+    register({login, password}, callback) {
+        
+    }
+}
